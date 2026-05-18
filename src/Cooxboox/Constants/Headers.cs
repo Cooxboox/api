d457a18d57c5e7beddc2c916a7dee878531f30a8
Francis Pion <francispion@hotmail.com>
2026-05-17 21:09:27 -0400

    api

---
+++ src/Cooxboox/Constants/Headers.cs
@@ -0,0 +1,7 @@
+﻿namespace Cooxboox.Constants;
+
+internal static class Headers
+{
+  public const string ApiKey = "X-API-Key";
+  public const string Kitchen = "X-Kitchen";
+}
---
+++ src/Cooxboox/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Cooxboox;
+
+internal class Program
+{
+  public static void Main(string[] args)
+  {
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    IConfiguration configuration = builder.Configuration;
+
+    Startup startup = new(configuration);
+    startup.ConfigureServices(builder.Services);
+
+    WebApplication application = builder.Build();
+
+    startup.Configure(application);
+
+    application.Run();
+  }
+}
