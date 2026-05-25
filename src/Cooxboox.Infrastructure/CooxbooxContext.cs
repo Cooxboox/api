40a994864fdc4b204ecbcc9e4b16e224d4b699b9
Francis Pion <francispion@hotmail.com>
2026-05-25 14:39:23 -0400

    infra

---
+++ src/Cooxboox.Infrastructure/CooxbooxContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cooxboox.Infrastructure;
+
+public class CooxbooxContext : DbContext
+{
+  public CooxbooxContext(DbContextOptions<CooxbooxContext> options) : base(options)
+  {
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+  }
+}
---
+++ src/Cooxboox.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Logitar.EventSourcing.EntityFrameworkCore.Relational;
+using Logitar.EventSourcing.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cooxboox.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddCooxbooxInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddLogitarEventSourcingInfrastructure()
+      .AddLogitarEventSourcingWithEntityFrameworkCoreRelational();
+  }
+}
