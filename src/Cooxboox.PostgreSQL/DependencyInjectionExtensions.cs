3767b6dd0607c5d48e3c24641774a73f5f8bf72c
Francis Pion <francispion@hotmail.com>
2026-05-25 14:39:48 -0400

    db

---
+++ src/Cooxboox.PostgreSQL/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Cooxboox.Infrastructure;
+using Logitar;
+using Logitar.EventSourcing.EntityFrameworkCore.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cooxboox.PostgreSQL;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddCooxbooxPostgreSQL(this IServiceCollection services, IConfiguration configuration)
+  {
+    string? connectionString = EnvironmentHelper.TryGetString("POSTGRESQLCONNSTR_Cooxboox") ?? configuration.GetConnectionString("PostgreSQL");
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+      throw new InvalidOperationException("The PostgreSQL connection string was not found.");
+    }
+
+    return services
+      .AddDbContext<CooxbooxContext>(options => options.UseNpgsql(connectionString, options => options.MigrationsAssembly("Cooxboox.PostgreSQL")))
+      .AddLogitarEventSourcingWithEntityFrameworkCorePostgreSQL(connectionString);
+  }
+}
