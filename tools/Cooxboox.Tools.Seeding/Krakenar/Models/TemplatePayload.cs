764ff30cb3cebeefd2ea4070ec192e3f0a421c0b
Francis Pion <francispion@hotmail.com>
2026-05-25 22:48:27 -0400

    seed

---
+++ tools/Cooxboox.Tools.Seeding/Krakenar/Models/TemplatePayload.cs
@@ -0,0 +1,8 @@
+﻿using Krakenar.Contracts.Templates;
+
+namespace Cooxboox.Tools.Seeding.Krakenar.Models;
+
+internal record TemplatePayload : CreateOrReplaceTemplatePayload
+{
+  public Guid Id { get; set; }
+}
---
+++ tools/Cooxboox.Tools.Seeding/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Cooxboox.Tools.Seeding.Krakenar.Tasks;
+using Krakenar.Client;
+using Logitar.CQRS;
+
+namespace Cooxboox.Tools.Seeding;
+
+internal class Startup
+{
+  private readonly IConfiguration _configuration;
+
+  public Startup(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddHostedService<SeedingWorker>();
+    services.AddKrakenarClient(_configuration);
+    services.AddLogitarCQRS();
+
+    services.AddTransient<ICommandHandler<SeedDictionariesTask, Unit>, SeedDictionariesTaskHandler>();
+    services.AddTransient<ICommandHandler<SeedLanguagesTask, Unit>, SeedLanguagesTaskHandler>();
+    services.AddTransient<ICommandHandler<SeedRealmTask, Unit>, SeedRealmTaskHandler>();
+    services.AddTransient<ICommandHandler<SeedSendersTask, Unit>, SeedSendersTaskHandler>();
+    services.AddTransient<ICommandHandler<SeedTemplatesTask, Unit>, SeedTemplatesTaskHandler>();
+  }
+}
