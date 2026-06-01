d68ca7c7464ef8443d7ad97cffc69e7040e5cbd3
Francis Pion <francispion@hotmail.com>
2026-06-01 18:10:23 -0400

    Seed Kitchen Configuration. (#14)

---
+++ tools/Cooxboox.Tools.Seeding/Krakenar/Models/ContentTypePayload.cs
@@ -0,0 +1,10 @@
+﻿using Krakenar.Contracts.Contents;
+
+namespace Cooxboox.Tools.Seeding.Krakenar.Models;
+
+internal record ContentTypePayload : CreateOrReplaceContentTypePayload
+{
+  public Guid Id { get; set; }
+
+  public List<FieldDefinitionPayload> Fields { get; set; } = [];
+}
---
+++ tools/Cooxboox.Tools.Seeding/Krakenar/Models/FieldTypePayload.cs
@@ -0,0 +1,8 @@
+﻿using Krakenar.Contracts.Fields;
+
+namespace Cooxboox.Tools.Seeding.Krakenar.Models;
+
+internal record FieldTypePayload : CreateOrReplaceFieldTypePayload
+{
+  public Guid Id { get; set; }
+}
