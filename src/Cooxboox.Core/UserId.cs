5ee62da1514581b5469f3139d7190a8fc8850f01
Francis Pion <francispion@hotmail.com>
2026-06-02 17:11:52 -0400

    UserId

--- src/Cooxboox.Core/UserId.cs
+++ src/Cooxboox.Core/UserId.cs
@@ -1,4 +1,6 @@
-﻿using Krakenar.Contracts.Users;
+﻿using Cooxboox.Core.Actors;
+using Krakenar.Contracts.Actors;
+using Krakenar.Contracts.Users;
 using Logitar.EventSourcing;
 
 namespace Cooxboox.Core;
@@ -8,21 +10,43 @@ public readonly struct UserId
   public ActorId ActorId { get; }
   public string Value => ActorId.Value;
 
+  public Guid? RealmId { get; }
+  public Guid EntityId { get; }
+
   public UserId(ActorId actorId)
   {
+    Actor actor = actorId.ToActor();
+    if (actor.Type != ActorType.User)
+    {
+      throw new ArgumentException("The actor must be a User.", nameof(actorId));
+    }
+
     ActorId = actorId;
 
-    // TODO(fpion): RealmId
-    // TODO(fpion): EntityId
+    RealmId = actor.RealmId;
+    EntityId = actor.Id;
   }
 
   public UserId(string value) : this(new ActorId(value))
   {
   }
 
-  public UserId(User user)
+  public UserId(Guid entityId, Guid? realmId = null)
   {
-    // TODO(fpion): implement
+    Actor actor = new()
+    {
+      RealmId = realmId,
+      Type = ActorType.User,
+      Id = entityId
+    };
+    ActorId = actor.ToActorId();
+
+    RealmId = realmId;
+    EntityId = entityId;
+  }
+
+  public UserId(User user) : this(user.Id, user.Realm?.Id)
+  {
   }
 
   public static bool operator ==(UserId left, UserId right) => left.Equals(right);
