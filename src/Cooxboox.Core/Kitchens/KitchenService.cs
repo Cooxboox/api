ca03f9fa93a100f8dc7e7f4b098f2f0f8f1a9b5c
Francis Pion <francispion@hotmail.com>
2026-06-02 10:30:29 -0400

    read_update

--- src/Cooxboox.Core/Kitchens/KitchenService.cs
+++ src/Cooxboox.Core/Kitchens/KitchenService.cs
@@ -1,5 +1,6 @@
 ﻿using Cooxboox.Core.Kitchens.Commands;
 using Cooxboox.Core.Kitchens.Models;
+using Cooxboox.Core.Kitchens.Queries;
 using Logitar.CQRS;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,8 @@ namespace Cooxboox.Core.Kitchens;
 public interface IKitchenService
 {
   Task<CreateOrReplaceKitchenResult> CreateOrReplaceAsync(CreateOrReplaceKitchenPayload payload, Guid? id = null, CancellationToken cancellationToken = default);
+  Task<KitchenModel?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
+  Task<KitchenModel?> UpdateAsync(Guid id, UpdateKitchenPayload payload, CancellationToken cancellationToken = default);
 }
 
 internal class KitchenService : IKitchenService
@@ -16,18 +19,34 @@ internal class KitchenService : IKitchenService
   {
     services.AddTransient<IKitchenService, KitchenService>();
     services.AddTransient<ICommandHandler<CreateOrReplaceKitchenCommand, CreateOrReplaceKitchenResult>, CreateOrReplaceKitchenCommandHandler>();
+    services.AddTransient<ICommandHandler<UpdateKitchenCommand, KitchenModel?>, UpdateKitchenCommandHandler>();
+    services.AddTransient<IQueryHandler<ReadKitchenQuery, KitchenModel?>, ReadKitchenQueryHandler>();
   }
 
   private readonly ICommandBus _commandBus;
+  private readonly IQueryBus _queryBus;
 
-  public KitchenService(ICommandBus commandBus)
+  public KitchenService(ICommandBus commandBus, IQueryBus queryBus)
   {
     _commandBus = commandBus;
+    _queryBus = queryBus;
   }
 
   public async Task<CreateOrReplaceKitchenResult> CreateOrReplaceAsync(CreateOrReplaceKitchenPayload payload, Guid? id, CancellationToken cancellationToken)
   {
     CreateOrReplaceKitchenCommand command = new(payload, id);
+    return await _commandBus.ExecuteAsync(command, cancellationToken);
+  }
+
+  public async Task<KitchenModel?> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    ReadKitchenQuery query = new(id);
+    return await _queryBus.ExecuteAsync(query, cancellationToken);
+  }
+
+  public async Task<KitchenModel?> UpdateAsync(Guid id, UpdateKitchenPayload payload, CancellationToken cancellationToken)
+  {
+    UpdateKitchenCommand command = new(id, payload);
     return await _commandBus.ExecuteAsync(command, cancellationToken);
   }
 }
