82bcaaa2f3aab3fb47296738487c99f6bdef39aa
Francis Pion <francispion@hotmail.com>
2026-06-07 22:13:21 -0400

    converters

---
+++ src/Cooxboox.Infrastructure/Converters/NotesConverter.cs
@@ -0,0 +1,16 @@
+﻿using Cooxboox.Core;
+
+namespace Cooxboox.Infrastructure.Converters;
+
+internal class NotesConverter : JsonConverter<Notes>
+{
+  public override Notes? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    return Notes.TryCreate(reader.GetString());
+  }
+
+  public override void Write(Utf8JsonWriter writer, Notes notes, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(notes.Value);
+  }
+}
